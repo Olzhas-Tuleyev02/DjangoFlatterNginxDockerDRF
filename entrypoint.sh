c13da23f0ddfa79e2d0d97871da0ce7902b13477
lvlit <player14017@gmail.com>
2025-11-10 11:39:28 +0500

    FIX фвьшт

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-# Ожидание доступности базы данных
-# python manage.py shell -c "from django.db import connections; from django.db.utils import OperationalError; import time; db_conn = None; while not db_conn:;     try:;         db_conn = connections[\"default\"];     except OperationalError:;         print(\"Database unavailable, waiting 1 second...\");         time.sleep(1);" 
 
-# Применение миграций базы данных
-# echo "Applying database migrations..."
-# python manage.py migrate
+# ╨а╤Ы╨а┬╢╨а╤С╨а╥С╨а┬░╨а╨Е╨а╤С╨а┬╡ ╨а╥С╨а╤Х╨б╨Г╨бтАЪ╨б╤У╨а╤Ч╨а╨Е╨а╤Х╨б╨Г╨бтАЪ╨а╤С ╨а┬▒╨а┬░╨а┬╖╨бтА╣ ╨а╥С╨а┬░╨а╨Е╨а╨Е╨бтА╣╨бтАж
+python manage.py shell -c "from django.db import connections; from django.db.utils import OperationalError; import time; db_conn = None; while not db_conn:;     try:;         db_conn = connections[\"default\"];     except OperationalError:;         print(\"Database unavailable, waiting 1 second...\");         time.sleep(1);"
 
-# Сбор статических файлов
-# echo "Collecting static files..."
-# python manage.py collectstatic --noinput
+# ╨а╤Я╨б╨В╨а╤С╨а╤Ш╨а┬╡╨а╨Е╨а┬╡╨а╨Е╨а╤С╨а┬╡ ╨а╤Ш╨а╤С╨а╤Ц╨б╨В╨а┬░╨бтАа╨а╤С╨атДЦ ╨а┬▒╨а┬░╨а┬╖╨бтА╣ ╨а╥С╨а┬░╨а╨Е╨а╨Е╨бтА╣╨бтАж
+echo "Applying database migrations..."
+python manage.py migrate
 
-# Создание суперпользователя, если он не существует
-# echo "Creating superuser if it does not exist..."
-# python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+# ╨а╨О╨а┬▒╨а╤Х╨б╨В ╨б╨Г╨бтАЪ╨а┬░╨бтАЪ╨а╤С╨бтАб╨а┬╡╨б╨Г╨а╤Ф╨а╤С╨бтАж ╨бтАЮ╨а┬░╨атДЦ╨а┬╗╨а╤Х╨а╨Ж
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
-# Запуск Gunicorn
+# ╨а╨О╨а╤Х╨а┬╖╨а╥С╨а┬░╨а╨Е╨а╤С╨а┬╡ ╨б╨Г╨б╤У╨а╤Ч╨а┬╡╨б╨В╨а╤Ч╨а╤Х╨а┬╗╨б╨К╨а┬╖╨а╤Х╨а╨Ж╨а┬░╨бтАЪ╨а┬╡╨а┬╗╨б╨П, ╨а┬╡╨б╨Г╨а┬╗╨а╤С ╨а╤Х╨а╨Е ╨а╨Е╨а┬╡ ╨б╨Г╨б╤У╨бтА░╨а┬╡╨б╨Г╨бтАЪ╨а╨Ж╨б╤У╨а┬╡╨бтАЪ
+echo "Creating superuser if it does not exist..."
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+
+# ╨атАФ╨а┬░╨а╤Ч╨б╤У╨б╨Г╨а╤Ф Gunicorn
 echo "Starting Gunicorn..."
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
